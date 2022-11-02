c188b7946f181c0c0f4c360a9b7c1896afc63a2c
Wang Shida <wangs@fpcomplete.com>
2022-11-02 19:06:58 +0800

    handlerBlock

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,26 +1,33 @@
-import { Vote } from "../types";
-import { CosmosMessage } from "@subql/types-cosmos";
+import { ActiveEraInfo, Balance, EraIndex, Exposure } from "@polkadot/types/interfaces";
+import { Option } from "@polkadot/types"
+import { SubstrateEvent } from "@subql/types";
+import { Validator } from "../types";
 
-type Msg = {
-  vote: {
-    proposal_id: bigint;
-    vote: string;
-  };
-};
-
-export async function handleTerraDeveloperFund(
-  message: CosmosMessage<{ sender: string; msg: Msg }>
-): Promise<void> {
-  // logger.info(JSON.stringify(message));
-  // Example vote https://www.mintscan.io/juno/txs/EAA2CC113B3EC79AE5C280C04BE851B82414B108273F0D6464A379D7917600A4
-
-  const voteRecord = Vote.create({
-    id: `${message.tx.hash}-${message.idx}`,
-    blockHeight: BigInt(message.block.block.header.height),
-    voter: message.msg.decodedMsg.sender,
-    proposalID: message.msg.decodedMsg.msg.vote.proposal_id,
-    vote: message.msg.decodedMsg.msg.vote.vote === "yes",
-  });
-
-  await voteRecord.save();
+export async function handleBlock({ block }: SubstrateEvent): Promise<void> {
+  // in the early stage of kusama, staking.activeEra didn't exist
+  if (!api.query.staking.activeEra) return;
+  const [activeEra] = await api.queryMulti<[Option<ActiveEraInfo>, Option<EraIndex>]>([
+    api.query.staking.activeEra,
+    // api.query.staking.currentEra
+  ]);
+  if (activeEra.isEmpty) return;
+  const entity = new ValidatorThreshold(activeEra.unwrap().index.toString());
+  const validators = await api.query.session.validators();
+  const exposureInfos = await api.queryMulti<Exposure[]>(
+    validators.map(validator => [api.query.staking.erasStakersClipped, [activeEra.unwrap().index, validator]])
+  );
+  const thresholdValidator = exposureInfos.reduce<{ accountId: string, total: Balance }>((acc, exposure, idx) => {
+    if (!acc || exposure.total.unwrap().lt(acc.total)) {
+      return { accountId: validators[idx].toString(), total: exposure.total.unwrap() };
+    }
+    return acc;
+  }, undefined);
+  entity.startBlock = block.block.header.number.toNumber();
+  entity.timestamp = block.timestamp;
+  entity.totalValidators = validators.length;
+  entity.validatorWithLeastBond = thresholdValidator.accountId;
+  entity.leastStaked = thresholdValidator.total.toBigInt();
+  entity.totalStaked = (await api.query.staking.erasTotalStake(activeEra.unwrap().index)).toBigInt();
+  entity.maxNominatorRewardedPerValidator = api.consts.staking.maxNominatorRewardedPerValidator?.toNumber();
+  await entity.save();
 }
